917b8ea0fe30aaf199ac6143af70724119d470be
Josh Baum <joshbaum@google.com>
2020-11-01 13:42:12 -0500

    final'

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
 // TODO: REPLACE YOUR TOKEN
-var apiToken = "?token=YOUR_TOKEN_HERE";
+var apiToken = "?token=4QUt-dLizN6NO8PXF2HUHABnkDXE9Ut8Oe4C7CqfKgY";
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -20,12 +20,32 @@ const corsPromise = () =>
     resolve(request);
   });
 
-// THIS IS SOME SAMPLE CODE FOR HOW TO USE PROMISES -- feel free to adapt this into a function!
+
 corsPromise().then(
   (request) =>
     (request.onload = request.onerror = function () {
-      // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
+      const plantData = JSON.parse(request.response).data;
+      addData(plantData);
     })
 );
 
-//// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
+const addData = (plants) => {
+  const goodPlants = plants.filter(plant => plant.year == 1753);
+  goodPlants.forEach(plant => {
+    putPlant(plant);
+  });
+}
+
+const putPlant = (plant) => {
+  console.log(plant)
+  let newDiv = document.createElement('div');
+  let title = document.createElement('h3');
+  title.innerHTML = plant.common_name;
+  let pic = document.createElement('img');
+  pic.setAttribute("src", plant.image_url);
+  newDiv.appendChild(title);
+  newDiv.appendChild(pic);
+  document.getElementById('plants').appendChild(newDiv);
+}
+
+
